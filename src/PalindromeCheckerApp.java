b7ece4d056c86f49aae0fbe76b5c9c54534dc8f0
joshhaydenkings <joshuvahayden138@gmail.com>
2026-02-23 09:24:57 +0530

    Apllication Entry setup

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,12 @@
+public class PalindromeCheckerApp {
+
+    //Application entry point
+    public static void main(String[] args) {
+
+        System.out.println("-------------------------------------");
+        System.out.println(" Welcome to Palindrome Checker App ");
+        System.out.println(" Application Version: 1.0 ");
+        System.out.println("-------------------------------------");
+
+    }
+}
