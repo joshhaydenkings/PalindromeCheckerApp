3bbc5e98f7dee9c41c27d394e927bd768731de48
joshhaydenkings <joshuvahayden138@gmail.com>
2026-02-23 11:17:06 +0530

    UC2:Hardcoded palindrome

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,12 +1,18 @@
-public class PalindromeCheckerApp {
+public class UC2PalindromeCheckerApp {
 
-    //Application entry point
     public static void main(String[] args) {
 
-        System.out.println("-------------------------------------");
-        System.out.println(" Welcome to Palindrome Checker App ");
-        System.out.println(" Application Version: 1.0 ");
-        System.out.println("-------------------------------------");
+        String input = "dad
+        String reverse = "";
 
+        for (int i = input.length() - 1; i >= 0; i--) {
+            reverse = reverse + input.charAt(i);
+        }
+
+        if (input.equals(reverse)) {
+            System.out.println("The string \"" + input + "\" is a Palindrome.");
+        } else {
+            System.out.println("The string \"" + input + "\" is NOT a Palindrome.");
+        }
     }
 }
